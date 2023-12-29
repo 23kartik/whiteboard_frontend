40ec628afbc5f10e7adc17c40e1ee69c15a1f4ea
23kartik <kartik23tiwari@gmai.com>
2023-12-29 23:25:05 +0530

    added svg and corrected them

--- src/components/DrawingCanvas.js
+++ src/components/DrawingCanvas.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useDraw } from '../hooks/useDraw';
 import { ChromePicker } from 'react-color';
-import { FaPalette, FaTrashAlt, FaSave, FaFileExport, FaPencilAlt ,FaMinus, FaPlus} from 'react-icons/fa';
+import {
+  FaPalette,
+  FaTrashAlt,
+  FaSave,
+  FaFileExport,
+  FaPencilAlt,
+  FaEraser,
+  FaPlus,
+  FaMinus,
+} from 'react-icons/fa';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaintBrush, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-
+import { faPaintBrush } from '@fortawesome/free-solid-svg-icons';
 import api from '../service/api';
 import { drawLine } from '../utils/drawLine';
 import { io } from 'socket.io-client';
@@ -12,34 +20,36 @@ import { io } from 'socket.io-client';
 const socket = io('http://localhost:5001');
 
 const DrawingCanvas = ({ user }) => {
- 
   const [color, setColor] = useState('#000');
   const [showColorPicker, setShowColorPicker] = useState(false);
   const [lineWidth, setLineWidth] = useState(5);
   const [canvasBackground, setCanvasBackground] = useState('#ffffff');
+  const [eraserMode, setEraserMode] = useState(false);
 
+
+  
   const createLine = ({ prevPoint, currentPoint, ctx }) => {
-    socket.emit('draw-line', { prevPoint, currentPoint, color, lineWidth }); // Pass lineWidth to drawLine
-    drawLine({ prevPoint, currentPoint, ctx, color, lineWidth }); // Pass lineWidth to drawLine
+    const lineColor = eraserMode ? canvasBackground : color;
+    socket.emit('draw-line', { prevPoint, currentPoint, color: lineColor, lineWidth });
+    drawLine({ prevPoint, currentPoint, ctx, color: lineColor, lineWidth });
   };
-
 
   const exportDrawing = () => {
     try {
       const tempCanvas = document.createElement('canvas');
       const tempCtx = tempCanvas.getContext('2d');
-  
+
       // Set the dimensions of the temporary canvas
       tempCanvas.width = canvasRef.current.width;
       tempCanvas.height = canvasRef.current.height;
-  
+
       // Draw the background color onto the temporary canvas
       tempCtx.fillStyle = canvasBackground; // Set the background color
       tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
-  
+
       // Draw the current canvas content onto the temporary canvas
       tempCtx.drawImage(canvasRef.current, 0, 0);
-  
+
       // Use toBlob with a callback to get the Blob data
       tempCanvas.toBlob((blob) => {
         // Create a download link
@@ -47,10 +57,10 @@ const DrawingCanvas = ({ user }) => {
         link.href = URL.createObjectURL(blob);
         link.download = 'drawing.png'; // Set the desired file name and format
         document.body.appendChild(link);
-  
+
         // Trigger the download
         link.click();
-  
+
         // Cleanup
         document.body.removeChild(link);
       });
@@ -64,13 +74,12 @@ const DrawingCanvas = ({ user }) => {
   };
 
   const decreaseLineWidth = () => {
-    setLineWidth((prevWidth) =>  prevWidth - 1);
+    setLineWidth((prevWidth) => (prevWidth > 1 ? prevWidth - 1 : 1));
   };
 
-  const handleCanvasBackgroundChange = (newColor) => {
-    setCanvasBackground(newColor);
-  }; 
-
+  const switchEraserMode = () => {
+    setEraserMode((prevMode) => !prevMode);
+  };
 
   const { canvasRef, onMouseDown, clear } = useDraw(createLine);
 
@@ -97,7 +106,9 @@ const DrawingCanvas = ({ user }) => {
       drawLine({ prevPoint, currentPoint, ctx, color, lineWidth });
     });
 
-    socket.on('clear', clear);
+    socket.on('clear', () => {
+      clear();
+    });
 
     return () => {
       socket.off('draw-line');
@@ -125,7 +136,7 @@ const DrawingCanvas = ({ user }) => {
       console.error('Error saving drawing:', error);
     }
   };
-  console.log(lineWidth)
+
   const loadDrawing = async () => {
     try {
       const email = user.email;
@@ -153,77 +164,80 @@ const DrawingCanvas = ({ user }) => {
 
   return (
     <div className='max-h-screen flex items-center justify-center relative'>
-    <div className='flex flex-row  space-x-4 p-6 relative z-10'>
-      <div className='flex flex-col items-center space-y-4 relative'>
-        <button
-          type='button'
-          className='bg-gray-300 p-2 rounded-md border border-gray-400 hover:bg-gray-400'
-          onClick={() => setShowColorPicker(!showColorPicker)}
-        >
-          <FaPalette style={{ fontSize: '.6cm' }} />
-        </button>
-        {showColorPicker && (
-          <>
-            <div
-              className='fixed top-0 left-0 w-full h-full bg-black opacity-50'
-              onClick={() => setShowColorPicker(false)}
-            />
-            <div className='absolute top-0 left-full'>
-              <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
-            </div>
-          </>
-        )}
-        <button
-          type='button'
-          className='bg-red-400 p-2 rounded-md border border-gray-400 hover:bg-gray-400'
-          onClick={() => socket.emit('clear')}
-        >
-          <FaTrashAlt style={{ fontSize: '.6cm' }} />
-        </button>
-        <button
-          type='button'
-          className='bg-green-400 text-white p-2 rounded-md hover:bg-blue-600'
-          onClick={saveDrawing}
-        >
-          <FaSave style={{ fontSize: '.6cm' }} />
-        </button>
-     
-        <button
-          type='button'
-          className='bg-yellow-400 p-2 rounded-md border border-gray-400 hover:bg-gray-400'
-          onClick={() => exportDrawing()}
-        >
-          <FaFileExport style={{ fontSize: '.6cm' }}/>
-        </button>
-        <button
-  type='button'
-  className='bg-blue-400 p-[6px] rounded-md border border-gray-400 hover:bg-gray-400'
-  onClick={increaseLineWidth}
->
-  <FontAwesomeIcon icon={faPaintBrush} style={{ fontSize: '.6cm' }} />
-  <FaPlus className='ml-3'  style={{ fontSize: '.3cm' }} />
-  
-</button>
+      <div className='flex flex-row  space-x-4 p-6 relative z-10'>
+        <div className='flex flex-col items-center space-y-4 relative'>
+          <button
+            type='button'
+            className={`bg-${eraserMode ? 'gray' : 'blue'}-400 p-2 rounded-md border border-gray-400 hover:bg-gray-400 `}        
+                onClick={switchEraserMode}
+          >
+            {eraserMode ? <FaEraser style={{ fontSize: '.6cm' }} /> : <FaPencilAlt style={{ fontSize: '.6cm' }} />}
+          </button>
+          <button
+            type='button'
+            className='bg-gray-300 p-2 rounded-md border border-gray-400 hover:bg-gray-400'
+            onClick={() => setShowColorPicker(!showColorPicker)}
+          >
+            <FaPalette style={{ fontSize: '.6cm' }} />
+          </button>
+          {showColorPicker && (
+            <>
+              <div
+                className='fixed top-0 left-0 w-full h-full bg-black opacity-50'
+                onClick={() => setShowColorPicker(false)}
+              />
+              <div className='absolute top-0 left-full'>
+                <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
+              </div>
+            </>
+          )}
+          <button
+            type='button'
+            className='bg-red-400 p-2 rounded-md border border-gray-400 hover:bg-gray-400'
+            onClick={() => socket.emit('clear')}
+          >
+            <FaTrashAlt style={{ fontSize: '.6cm' }} />
+          </button>
+          <button
+            type='button'
+            className='bg-green-400 text-white p-2 rounded-md hover:bg-blue-600'
+            onClick={saveDrawing}
+          >
+            <FaSave style={{ fontSize: '.6cm' }} />
+          </button>
 
-<button
-  type='button'
-  className='bg-red-400 p-[6px] rounded-md border border-gray-400 hover:bg-gray-400'
-  onClick={decreaseLineWidth}
->
-<FontAwesomeIcon icon={faPaintBrush} style={{ fontSize: '.6cm' }} />
-<FaMinus className='ml-3' style={{ fontSize: '.3cm' }} />
-  
-  
-  
-</button>
+          <button
+            type='button'
+            className='bg-yellow-400 p-2 rounded-md border border-gray-400 hover:bg-gray-400'
+            onClick={() => exportDrawing()}
+          >
+            <FaFileExport style={{ fontSize: '.6cm' }} />
+          </button>
 
-      </div>
-     
-      <div className='border rounded-[2rem] shadow-xl -mt-5 w-[1355px] h-[685px] ' style={{  background: canvasBackground}}>
-      <canvas className="pencil-canvas" ref={canvasRef} onMouseDown={onMouseDown} width={1355} height={685} />
+          <button
+            type='button'
+            className='bg-blue-400 p-[6px] rounded-md border border-gray-400 hover:bg-gray-400'
+            onClick={increaseLineWidth}
+          >
+            <FontAwesomeIcon icon={faPaintBrush} style={{ fontSize: '.6cm' }} />
+            <FaPlus className='ml-3' style={{ fontSize: '.3cm' }} />
+          </button>
+
+          <button
+            type='button'
+            className='bg-red-400 p-[6px] rounded-md border border-gray-400 hover:bg-gray-400'
+            onClick={decreaseLineWidth}
+          >
+            <FontAwesomeIcon icon={faPaintBrush} style={{ fontSize: '.6cm' }} />
+            <FaMinus className='ml-3' style={{ fontSize: '.3cm' }} />
+          </button>
+        </div>
+
+        <div className='border rounded-[2rem] shadow-xl -mt-5 w-[1355px] h-[685px] ' style={{ background: canvasBackground }}>
+          <canvas className={`${eraserMode ? 'eraser-cursor' : 'pencil-canvas'}`} ref={canvasRef} onMouseDown={onMouseDown} width={1355} height={685} />
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
