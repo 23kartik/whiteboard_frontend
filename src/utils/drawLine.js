2008d77fce32ced7c52fbd3ddc252278a935e26c
23kartik <kartik23tiwari@gmai.com>
2023-12-29 20:13:47 +0530

    updated client to handle the change of lineWidth, and many other functionalities

--- src/utils/drawLine.js
+++ src/utils/drawLine.js
@@ -1,19 +1,20 @@
-export const drawLine = ({ prevPoint, currentPoint, ctx, color }) => {
-    const { x: currX, y: currY } = currentPoint;
-    const lineColor = color;
-    const lineWidth = 5;
-  
-    let startPoint = prevPoint ?? currentPoint;
-    ctx.beginPath();
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = lineColor;
-    ctx.moveTo(startPoint.x, startPoint.y);
-    ctx.lineTo(currX, currY);
-    ctx.stroke();
-  
-    ctx.fillStyle = lineColor;
-    ctx.beginPath();
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
-    ctx.fill();
-  };
-  
+export const drawLine = ({ prevPoint, currentPoint, ctx, color, lineWidth }) => {
+  const { x: currX, y: currY } = currentPoint;
+  const lineColor = color;
+
+  let startPoint = prevPoint ?? currentPoint;
+  ctx.beginPath();
+  ctx.lineWidth = lineWidth; // Use the dynamic line width
+  ctx.lineJoin = 'round'; // Set line join to round for smooth corners
+  ctx.lineCap = 'round'; // Set line cap to round for smooth ends
+
+  ctx.strokeStyle = lineColor;
+  ctx.moveTo(startPoint.x, startPoint.y);
+  ctx.lineTo(currX, currY);
+  ctx.stroke();
+
+  ctx.fillStyle = lineColor;
+  ctx.beginPath();
+  ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+  ctx.fill();
+};
